294213459bac8e59815185968027a8273f24ed2c
Eldar <147965602+eldar-05@users.noreply.github.com>
2024-01-28 00:30:40 +0600

    Update Main.java and fixing some bags

--- Main.java
+++ Main.java
@@ -6,13 +6,27 @@ public class Main
     {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        boolean gameIsRunning = true;
-        int randomIndexOfWord = random.nextInt(9);
+        boolean isGameRunning = true;
+        int randomIndexOfWord = random.nextInt(10);
+        System.out.print("How many players? 2,3,4... ");
+        int howManyPlayers = scanner.nextInt();
+        scanner.nextLine();
+        String[] namesOfPlayers = new String[howManyPlayers];
+        for (int i = 0; i < howManyPlayers; i++){
+            if(namesOfPlayers[i] != " "){
+                System.out.print("Player " + (i + 1) + ": ");
+                namesOfPlayers[i] = scanner.nextLine();
+            }
+        }
+        int maximumScore = getMaxPossibleScore(wordChoosed(randomIndexOfWord));  //max score calculator
         clear();
-        while (gameIsRunning)
+        while (isGameRunning)
         {
-            System.out.print("Hint: " + wordsMeanigsChoosed(randomIndexOfWord));
-            gameIsRunning = false;
+            System.out.println("Hint: " + wordsMeanigsChoosed(randomIndexOfWord));
+            for (int i = 0; i < howManyPlayers; i++){
+                System.out.println("Player " + (i + 1) + ": " + namesOfPlayers[i]);
+            }
+            isGameRunning = false;
         }
     }
     public static String wordChoosed (int randomIndexOfWord){
@@ -20,10 +34,28 @@ public class Main
         return words[randomIndexOfWord];
     }
     public static String wordsMeanigsChoosed (int randomIndexOfWord){
-        String[] meaningOfWord = {"A measure of steepness", "Thing that you take to show your knowledge", "the system of public works of a country", "The phenomenon in which the properties of an object or image are different from how they appear", "A suggestion that something is good or suitable for a particular purpose", "Creating and enforcing the rules of a society", "A place of business or residence with its furnishings and staff", "An act or process of staging or presenting a play, concert, or other form of entertainment", "A situation or condition favorable for attainment of a goal", "A measure of the average amount of energy of motion, or kinetic energy, a system contains"};
-        return meaningOfWord[randomIndexOfWord];
+        String[] meaningOfWords = {"A measure of steepness", "Thing that you take to show your knowledge", "the system of public works of a country", "The phenomenon in which the properties of an object or image are different from how they appear", "A suggestion that something is good or suitable for a particular purpose", "Creating and enforcing the rules of a society", "A place of business or residence with its furnishings and staff", "An act or process of staging or presenting a play, concert, or other form of entertainment", "A situation or condition favorable for attainment of a goal", "A measure of the average amount of energy of motion, or kinetic energy, a system contains"};
+        return meaningOfWords[randomIndexOfWord];
     }
     public static void clear(){
-        System.out.println("\033[H\033[2J");
+        System.out.print("\033[H\033[2J"); 
+    }
+    public static int getMaxPossibleScore(String word){
+        int max = 1;
+        String uniq = "" + word.charAt(0);
+        for (int i = 1; i < word.length(); i++) {
+            boolean found = false;
+            for (int j = 0; j < uniq.length(); j++) {
+                if (word.charAt(i) == uniq.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                uniq += word.charAt(i);
+                max++;
+            }
+        }
+        return max * 100;
     }
 }
